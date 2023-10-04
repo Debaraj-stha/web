3a3da61aa8651aa03779ef6cffb9676ff527cc14
Debaraj-stha <dstha221@gmail.com>
2023-10-04 13:38:28 +0545

    events in js

---
+++ public/js/events.js
@@ -0,0 +1,11 @@
+function clickEvent() {
+  console.log("click event");
+  alert("you clicked  the button");
+}
+function changeContent() {
+  let p1 = document.getElementById("p1");
+  p1.innerHTML = "you clicked the button to change content";
+}
+function hoverEvent() {
+  alert("you hover the button");
+}
